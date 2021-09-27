e3225f80de1a87ba54dbc91426208b40a2038c86
ALexis Moya <al.moyac@duocuc.cl>
2021-09-27 20:05:05 -0300

    creando pagina servicios

--- TallerMecanico/src/app/app-routing.module.ts
+++ TallerMecanico/src/app/app-routing.module.ts
@@ -22,7 +22,11 @@ const routes: Routes = [
   {
     path: 'recuperar',
     loadChildren: () => import('./recuperar/recuperar.module').then( m => m.RecuperarPageModule)
+  },  {
+    path: 'servicios',
+    loadChildren: () => import('./servicios/servicios.module').then( m => m.ServiciosPageModule)
   },
+
 
 ];
 
