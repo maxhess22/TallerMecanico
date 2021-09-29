d10d6bab40d05d99543747ef2504a23d608194a8
ayline acevedo <aylineacevedo20@gmail.com>
2021-09-29 10:21:50 -0300

    agendas

--- TallerMecanico/src/app/app-routing.module.ts
+++ TallerMecanico/src/app/app-routing.module.ts
@@ -22,7 +22,11 @@ const routes: Routes = [
   {
     path: 'recuperar',
     loadChildren: () => import('./recuperar/recuperar.module').then( m => m.RecuperarPageModule)
+  },  {
+    path: 'agendas',
+    loadChildren: () => import('./agendas/agendas.module').then( m => m.AgendasPageModule)
   },
+
 
 ];
 
