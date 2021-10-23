e9340f7f874584c8b446b87bbed2dbcc4bf95ab6
CristianSLL <cristiansilvallanquin@gmail.com>
2021-10-23 20:10:11 -0300

    ok

--- TallerMecanico/src/app/app.module.ts
+++ TallerMecanico/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { RouterModule } from '@angular/router';
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
@@ -11,15 +11,11 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent,
-    ProfileComponent ],
+  declarations: [AppComponent ],
   entryComponents: [],
   imports: [BrowserModule,
      IonicModule.forRoot(),
-      AppRoutingModule, IonicStorageModule.forRoot(), RouterModule.forRoot([
-        { path: '', redirectTo: '/', pathMatch: 'full' },
-        { path: 'login', component: LoginComponent },
-        { path: 'profile', component: ProfileComponent }],;
+      AppRoutingModule, IonicStorageModule.forRoot()],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
