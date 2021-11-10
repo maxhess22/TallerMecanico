1794a15ae85562397e5f00caed2184228fa3cde1
victfuentess <vict.fuentess@duocuc.cl>
2021-11-10 19:50:13 -0300

    borrado

--- api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Persona, Agenda, Servicio, Boleta
-# Register your models here.
-
-admin.site.register(Persona)
-admin.site.register(Agenda)
-admin.site.register(Servicio)
-admin.site.register(Boleta)
-
