5a52e540a1b0511add2cc894e17d3047e1a17a7f
victfuentess <vict.fuentess@duocuc.cl>
2021-11-11 19:38:49 -0300

    cambio login

--- api/views.py
+++ api/views.py
@@ -53,7 +53,8 @@ class ListPersonas(APIView):
         serializer = PersonaSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+            return Response( serializer.data)
+            #return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
         else:
             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
