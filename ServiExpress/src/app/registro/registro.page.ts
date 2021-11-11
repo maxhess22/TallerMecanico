bb111f944007a39043fc520ee11b25e985e4d595
victfuentess <vict.fuentess@duocuc.cl>
2021-11-11 12:28:33 -0300

    Registrar

---
+++ ServiExpress/src/app/registro/registro.page.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { UserCrudService } from '../services/user-crud.service';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.page.html',
+  styleUrls: ['./registro.page.scss'],
+})
+export class RegistroPage implements OnInit {
+
+  clienteForm: FormGroup;
+
+  constructor(
+    private router: Router,
+    public formBuilder: FormBuilder,
+    private zone: NgZone,
+    private userCrudService: UserCrudService) {
+    this.clienteForm = this.formBuilder.group({
+        nombreCliente: [''],
+        emailCliente: [''],
+        telefono: [''],
+        email: [''],
+        contraseña: ['']
+      })
+    }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    if (!this.clienteForm.valid) {
+      return false;
+    } else {
+      this.userCrudService.createCliente(this.clienteForm.value)
+        .subscribe((response) => {
+          this.zone.run(() => {
+            this.clienteForm.reset();
+            this.router.navigate(['/home']);
+          })
+        });
+    }
+  }
+
+}
---
+++ ServiExpress/src/app/services/user-crud.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+export class Cliente {
+  _id: number;
+  nombreCliente: string;
+  emailCliente: string;
+  telefono: number;
+  email: string;
+  contraseña: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserCrudService {
+
+  endpoint = 'https://serviexpress.pythonanywhere.com/api/cliente/';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Token beeb2c58df0f77b3ddd68e7ef1133a18995966d3' })
+  };
+
+
+  constructor(private httpClient: HttpClient) { }
+
+    createCliente(cliente: Cliente): Observable<any> {
+      return this.httpClient.post<Cliente>(this.endpoint, JSON.stringify(cliente), this.httpOptions)
+        .pipe(
+          catchError(this.handleError<Cliente>('Error occured'))
+        );
+    }
+
+    getCliente(id): Observable<Cliente[]> {
+      return this.httpClient.get<Cliente[]>(this.endpoint + '/' + id, this.httpOptions)
+        .pipe(
+          tap(_ => console.log(`Cliente fetched: ${id}`)),
+          catchError(this.handleError<Cliente[]>(`Get Cliente id=${id}`))
+        );
+    }
+
+    updateCliente(id, cliente: Cliente): Observable<any> {
+      return this.httpClient.patch(this.endpoint + '/' + id, JSON.stringify(cliente), this.httpOptions)
+        .pipe(
+          tap(_ => console.log(`Cliente updated: ${id}`)),
+          catchError(this.handleError<Cliente[]>('Update Cliente'))
+        );
+    }
+
+    deleteCliente(id): Observable<Cliente[]> {
+      return this.httpClient.delete<Cliente[]>(this.endpoint + '/' + id, this.httpOptions)
+        .pipe(
+          tap(_ => console.log(`Cliente deleted: ${id}`)),
+          catchError(this.handleError<Cliente[]>('Delete Cliente'))
+        );
+    }
+
+    private handleError<T>(operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+        console.error(error);
+        console.log(`${operation} failed: ${error.message}`);
+        return of(result as T);
+      };
+    }
+
+  }
